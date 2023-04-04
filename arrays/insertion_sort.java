037f29505751d5aa9b9da4fbc3da528ec15bd951
Aditya Kumar <aryan68125@gmail.com>
2023-04-04 19:12:48 +0530

    insertion sort edit

--- arrays/insertion_sort.java
+++ arrays/insertion_sort.java
@@ -92,17 +92,17 @@ _          */
 
         // Insert last element at its correct position
         // in sorted array.
-        int last = arr[n-1];
+        int temp = arr[n-1];
         int j = n-2;
 
         /* Move elements of arr[0..i-1], that are
           greater than key, to one position ahead
           of their current position */
-        while (j >= 0 && arr[j] > last)
+        while (j >= 0 && arr[j] > temp)
         {
             arr[j+1] = arr[j];
             j--;
         }
-        arr[j+1] = last;
+        arr[j+1] = temp;
     }
 }
